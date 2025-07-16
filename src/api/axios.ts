cd1a619ac473efa4101e1a00231cc254bd528162
aishwarya shinde <152557964+aishwaryagorakh@users.noreply.github.com>
2025-07-16 20:23:22 +0200

    add

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -11,7 +11,8 @@ import axios from 'axios';
 // export default api;
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:9000/api/auth',
+  // baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:9000/api/auth',
+   baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
