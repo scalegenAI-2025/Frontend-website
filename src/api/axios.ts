96ea4893af6abd470f9d507a8b8062b5f4e3b73e
aishwarya shinde <152557964+aishwaryagorakh@users.noreply.github.com>
2025-07-16 20:27:44 +0200

    add

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -12,7 +12,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:9000/api/auth',
-   baseURL: import.meta.env.VITE_API_BASE_URL,
+   baseURL: "https://backend-1-9qjs.onrender.com/api/auth",
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
