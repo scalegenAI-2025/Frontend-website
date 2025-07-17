f5d7f31b54303e387e47c33b344c0fc9f55a723d
aishwarya shinde <152557964+aishwaryagorakh@users.noreply.github.com>
2025-07-17 12:57:20 +0200

    add

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  
   server: {
     proxy: mode === 'development' ? {
       '/api': 'http://localhost:9000'
